83165e9abe03ed516438f861982b3d034e614a69
not-manu <manu.anish@mail.utoronto.ca>
2025-11-17 09:24:52 -0500

    styling

--- src/renderer/src/routes/layouts/RootLayout.tsx
+++ src/renderer/src/routes/layouts/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 export function RootLayout() {
   return (
-    <div className="p-4 bg-transparent text-foreground h-[100vh]">
+    <div className="p-4 bg-background/75 text-foreground h-[100vh] border-l dark:border-f-800/75 border-f-200/75">
       <Outlet />
     </div>
   )
